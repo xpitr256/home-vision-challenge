9918730f03dbe66fbe8598dcad1a55151547dde8
xpitr256 <pablomassuh@gmail.com>
2025-02-09 20:01:57 -0300

    #2: Converted the form image to black and white format for better processing

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"image"
+	"image/color"
 	_ "image/jpeg"
 	"log"
 	"net/http"
@@ -16,22 +17,53 @@ type Response struct {
 	ImageName string `json:"image_name"`
 }
 
-func getCheckboxes() (string, error) {
-	// TODO obtain the image from client side image upload
+func convertToBlackAndWhite(img image.Image) (*image.Gray, error) {
+	whiteColor := color.Gray{Y: 255}
+	blackColor := color.Gray{Y: 0}
+	response := image.NewGray(img.Bounds())
+	for y := 0; y < img.Bounds().Max.Y; y++ {
+		for x := 0; x < img.Bounds().Max.X; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			brightness := uint8((r*299 + g*587 + b*114) / 1000 >> 8)
+			// Less than 128 of brightness I considered it as "dark"
+			if brightness < 128 {
+				response.SetGray(x, y, blackColor)
+			} else {
+				response.SetGray(x, y, whiteColor)
+			}
+		}
+	}
+	return response, nil
+}
+
+func loadTestImage() (image.Image, string, error) {
 	filePath := "test/test-image.jpg"
 	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("error opening image:", err)
-		return "", errors.New("error opening image")
+		return nil, "", errors.New("error opening image")
 	}
 	fileName := filepath.Base(filePath)
 	defer file.Close()
 	formImage, _, err := image.Decode(file)
 	if err != nil {
-		fmt.Println("error decoding image:", err)
-		return "", errors.New("error decoding image")
+		return nil, "", errors.New("error decoding image")
 	}
-	fmt.Println("Image bounds: ", formImage.Bounds())
+	return formImage, fileName, nil
+}
+
+func getCheckboxes() (string, error) {
+	// TODO obtain the image from client side image upload
+	formImage, fileName, err := loadTestImage()
+	if err != nil {
+		fmt.Println("error loading test image:", err)
+		return "", err
+	}
+	blackAndWhiteImage, err := convertToBlackAndWhite(formImage)
+	if err != nil {
+		fmt.Println("error converting image to black and white:", err)
+		return "", err
+	}
+	fmt.Println(blackAndWhiteImage.Bounds())
 	return fileName, nil
 }
 
