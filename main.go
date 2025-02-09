560a54fbeefcc5a54495cb4a841b5aacf142f809
xpitr256 <pablomassuh@gmail.com>
2025-02-09 19:50:47 -0300

    #1: Read the jpg form image to start checkbox detection

--- main.go
+++ main.go
@@ -2,19 +2,50 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"image"
+	_ "image/jpeg"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 type Response struct {
-	Message string `json:"message"`
+	ImageName string `json:"image_name"`
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+func getCheckboxes() (string, error) {
+	// TODO obtain the image from client side image upload
+	filePath := "test/test-image.jpg"
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Println("error opening image:", err)
+		return "", errors.New("error opening image")
+	}
+	fileName := filepath.Base(filePath)
+	defer file.Close()
+	formImage, _, err := image.Decode(file)
+	if err != nil {
+		fmt.Println("error decoding image:", err)
+		return "", errors.New("error decoding image")
+	}
+	fmt.Println("Image bounds: ", formImage.Bounds())
+	return fileName, nil
+}
+
+func checkboxHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Response{Message: "Hello from Go API!"})
+	imageName, err := getCheckboxes()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Processing checkboxes"})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(Response{ImageName: imageName})
+
 }
 
 func main() {
@@ -23,7 +54,7 @@ func main() {
 		port = "8080"
 	}
 
-	http.HandleFunc("/", helloHandler)
+	http.HandleFunc("/checkbox", checkboxHandler)
 
 	fmt.Printf("Server running on port %s\n", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
