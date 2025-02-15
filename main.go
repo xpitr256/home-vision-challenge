172f704a2b2532698836c4c263823a9681102717
xpitr256 <pablomassuh@gmail.com>
2025-02-15 16:52:53 -0300

    #12: Added CORS support to allow cross-origin requests from frontend

--- main.go
+++ main.go
@@ -10,19 +10,39 @@ import (
 	"github.com/xpitr256/home-vision-challenge/controller"
 )
 
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
+	mux := http.NewServeMux()
+
 	// TODO migrate to POST request when receiving the image from client
-	http.HandleFunc("/checkbox", controller.CheckboxHandler)
+	mux.HandleFunc("/checkbox", controller.CheckboxHandler)
 
 	// Render api documentation
 	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
+	mux.Handle("/", fs)
+
+	handler := enableCORS(mux)
 
 	fmt.Printf("Server running on port %s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
